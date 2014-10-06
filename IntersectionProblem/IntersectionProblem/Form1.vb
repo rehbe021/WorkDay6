72beafb400c1a0cc540788b6a87e44bbd2992195
idsc3101 <idsc3101@gmail.com>
2014-10-06 15:25:29 -0500

    Completed workday 6

--- IntersectionProblem/IntersectionProblem/Form1.vb
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -18,16 +18,41 @@ Public Class Form1
             MessageBox.Show("Please enter valid numbers", "Error!")
             Return
         End If
-
+        
         ' Convert to double
         A = CDbl(Ast)
         B = CDbl(Bst)
         C = CDbl(Cst)
         D = CDbl(Dst)
 
-        ' Please write your code here...
+        If (B < A) Or (D < C) Then
+            MessageBox.Show("Please enter valid numbers")
+            Return
+        End If
 
-        txtResult.Text = "Not implemented yet"
+        txtResult.Text = FindIntersection(A, B, C, D)
+
     End Sub
+    Function FindIntersection(a As Double, b As Double, c As Double, d As Double) As String
+        Dim result As String
+        Dim int1, int2 As Double
+        If (b < c) Or (d < a) Then
+            result = "No intersection"
+        Else
+            If a < c Then
+                int1 = c
+            Else
+                int1 = a
+            End If
+            If d < b Then
+                int2 = d
+            Else
+                int2 = b
+            End If
+            result = "There is an intersection from " & int1 & " to " & int2
+        End If
 
+        Return result
+
+    End Function
 End Class
--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,15 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
-
-        txtHyp.Text = "Not Implemented Yet"
-
+        Dim result As Double
+        Dim a As Double = CDbl(txtSideOne.Text)
+        Dim b As Double = CDbl(txtSideTwo.Text)
+        result = hypotenuse(a, b)
+        txtHyp.Text = CStr(result)
     End Sub
-
+    Function hypotenuse(ByVal a As Double, ByVal b As Double) As Double
+        Dim c As Double
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+        Return c
+    End Function
 End Class
